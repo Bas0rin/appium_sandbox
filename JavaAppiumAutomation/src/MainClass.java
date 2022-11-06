526f8868a5e25d8f7494529c0099d1bbe779a918
Bas0rin <aleksey.basorin@gmail.com>
2022-11-06 18:36:58 +0300

    Homework for fiert lesson

--- JavaAppiumAutomation/src/MainClass.java
+++ JavaAppiumAutomation/src/MainClass.java
@@ -1,12 +1,30 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClass extends MainClassTest
 {
+    private int class_number = 45;
+    private String class_string = "world";
+
     @Test
     public void getLocalNumber()
     {
-        int a = this.testGetLocalNumber(14);
+        int a = testGetLocalNumber(14);
         System.out.println(a);
+    }
+
+    @Test
+    public void getClassNumber()
+    {
+        int b = testGetClassNumber(this.class_number);
+
+    }
+
+    @Test
+    public void getClassString()
+    {
+        String c = testGetClassString(this.class_string);
+
     }
 
 }
--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+import org.junit.internal.runners.statements.Fail;
 
 public class MainClassTest
 {
@@ -11,5 +13,35 @@ public class MainClassTest
             System.out.println("Fail getLocalNumber != 14");
         }
         return number;
+    }
+
+    public int testGetClassNumber (int number_2)
+    {
+        if (number_2 > 45)
+        {
+            System.out.println("classNumber > 45");
+        } else if (number_2 < 45)
+        {
+            System.out.println("classNumber < 45");
+        } else if (number_2 == 45) {
+            System.out.println("classNumber = 45");
+        }
+        return number_2;
+    }
+
+
+    public String testGetClassString (String str)
+    {
+        String substring = "Hello";
+        if (str.toLowerCase().contains(substring.toLowerCase()))
+        {
+            System.out.println("Class_string contains \"hello\" ");
+        } else if (str.toUpperCase().contains(substring.toUpperCase()))
+        {
+            System.out.println("Class_string contains \"Hello\" ");
+        } else {
+            Assert.fail("Class_string not contains \"Hello\" ");
+        }
+        return str;
     }
 }
