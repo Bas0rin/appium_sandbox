fe8b9abbfd69492bbef6eeb3351145fb1d3e79ed
Bas0rin <aleksey.basorin@gmail.com>
2022-11-06 16:58:35 +0300

    HM for test_1

---
+++ JavaAppiumAutomation/src/MainClass.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class MainClass extends MainClassTest
+{
+    @Test
+    public void getLocalNumber()
+    {
+        int a = this.testGetLocalNumber(14);
+        System.out.println(a);
+    }
+
+}
---
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -0,0 +1,15 @@
+
+public class MainClassTest
+{
+    public int testGetLocalNumber(int number)
+    {
+        if (number == 14)
+        {
+            System.out.println("Success getLocalNumber is 14");
+        } else if (number != 14)
+        {
+            System.out.println("Fail getLocalNumber != 14");
+        }
+        return number;
+    }
+}
